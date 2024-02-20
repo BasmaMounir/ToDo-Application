88a38a2be9e183726d9d14be509dce351868adb9
BasmaMounir <basmamounir57@gimal.com>
2024-02-20 14:53:56 +0200

    + splash screen + empty home screen + colors of light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_application/Home/home_screen.dart';
+import 'package:to_do_application/my_theme.dart';
+import 'package:to_do_application/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToDo Application',
+      debugShowCheckedModeBanner: false,
+      theme: MyTheme.lightTheme,
+      darkTheme: MyTheme.darkTheme,
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (_)=> const SplashScreen(),
+        HomeScreen.routeName: (_)=> const HomeScreen(),
+
+      },
     );
   }
 }
---
+++ lib/my_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyTheme{
+  static Color primaryColor = Color(0xff5D9CEC);
+  static Color lightBody = Color(0xffDFECDB);
+  static Color wightColor = Color(0xffFFFFFF);
+  static Color grayColor = Color(0xffC8C9CB);
+  static Color deepGrayColor = Color(0xff707070);
+  static Color blackColor = Color(0xff5383838);
+  static Color greenColor = Color(0xff61E757);
+  static Color redColor = Color(0xffEC4B4B);
+  static Color darkBlackColor = Color(0xff141922);
+  static Color darkBody = Color(0xff060E1E);
+
+
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: primaryColor,
+  );
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: primaryColor,
+  );
+
+}
